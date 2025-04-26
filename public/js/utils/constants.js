8016c74b071a946739fa3c8fcbdd8e911504dac2
Sander Nilsen <sander.nil@hotmail.com>
2025-04-26 21:06:02 +0200

    Remove constants.js from version control

--- public/js/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE = "https://v2.api.noroff.dev";
-export const API_LISTINGS = `/auction/listings?_seller=true`;
-export const API_KEY = "d94005fc-03c6-4fa1-89e9-9924d447980a"; 
-export const API_LOGIN = `${API_BASE}/auth/login`;
-export const API_REGISTER = `${API_BASE}/auth/register`;
